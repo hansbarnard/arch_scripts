8fd2a4c07102ab65c4e43c102a14c934531e2cc1
hansbarnard <johannes.barnard@clarivate.com>
2020-06-10 23:29:33 +0100

    ddd

--- install-gnome.sh
+++ install-gnome.sh
@@ -8,3 +8,14 @@ systemctl enable gdm.service 1>/dev/null
 echo "===== Installing gnome extra"
 selected_extra_packages=$(grep -i $'^Y\s' /scripts/gnome-extra-packages-selection.txt | awk -v ORS=' ' -F'\t' '{print $2}' )
 pacman -S $selected_extra_packages --noconfirm 1>/dev/null
+
+
+rm -f /usr/share/applications/avahi-discover.desktop
+rm -f /usr/share/applications/bvnc.desktop
+rm -f /usr/share/applications/bssh.desktop
+rm -f /usr/share/applications/org.freedesktop.MalcontentControl.desktop
+rm -f /usr/share/applications/stoken-gui.desktop
+rm -f /usr/share/applications/stoken-gui-small
+rm -f /usr/share/applications/qv4l2.desktop
+rm -f /usr/share/applications/qvidcap.desktop
+
--- user-install.sh
+++ user-install.sh
@@ -4,12 +4,13 @@ su hansbarnard <<EOSU
 
 echo "===== Installing yay"
 cd /tmp
-git clone https://aur.archlinux.org/yay.git 1> /dev/null
+git clone -q https://aur.archlinux.org/yay.git 1> /dev/null
 cd yay
 makepkg -si --noconfirm 1> /dev/null
 cd ~/
 
 echo "===== Installing google-chome"
 yay -Sy google-chrome --noconfirm 1> /dev/null
+
 
 EOSU
