89d1d9cffb5893d76d33648511562fcc80b0c012
hansbarnard <johannes.barnard@clarivate.com>
2020-06-11 17:29:50 +0100

    ddd

--- user-install.sh
+++ user-install.sh
@@ -3,42 +3,43 @@
 su hansbarnard <<EOSU
 
 echo =======Install SDKMan
-curl -s "https://get.sdkman.io" | bash  1> /dev/null
-source "$HOME/.sdkman/bin/sdkman-init.sh" 1> /dev/null
+curl -s "https://get.sdkman.io" | bash  &> /dev/null
+source "$HOME/.sdkman/bin/sdkman-init.sh" &> /dev/null
 sdk version
 
 echo =======Install java
-sdk install java 8.0.252-open 1> /dev/null
-
+sdk install java 8.0.252-open &> /dev/null
+java -version
 
 echo "===== Installing yay"
 cd /tmp
 git clone -q https://aur.archlinux.org/yay.git 1> /dev/null
 cd yay
-makepkg -si --noconfirm 1> /dev/null
+makepkg -si --noconfirm &> /dev/null
+yay --version
 cd ~/
 
 echo "===== Installing google-chome"
-yay -Sy google-chrome --noconfirm --answerclean No --nodiffmenu  1> /dev/null
+yay -Sy google-chrome --noconfirm --answerclean No --nodiffmenu  &> /dev/null
 
 echo "===== Installing postman"
-yay -Sy postman --noconfirm --answerclean No --nodiffmenu  1> /dev/null
+yay -Sy postman --noconfirm --answerclean No --nodiffmenu  &> /dev/null
 
 echo "===== Installing teams"
-yay -Sy teams --noconfirm --answerclean No --nodiffmenu  1> /dev/null
+yay -Sy teams --noconfirm --answerclean No --nodiffmenu  &> /dev/null
 
 echo "===== Installing FreeOffice"
-yay -Sy freeoffice --noconfirm --answerclean No --nodiffmenu  1> /dev/null
+yay -Sy freeoffice --noconfirm --answerclean No --nodiffmenu  &> /dev/null
 
 echo ===== Install OneDrive
-yay -Sqy onedrive-abraunegg --noconfirm --answerclean No --nodiffmenu 1> /dev/null
+yay -Sqy onedrive-abraunegg --noconfirm --answerclean No --nodiffmenu &> /dev/null
 
 
 echo ===== Install timeshift
-yay -Sqy timeshift --noconfirm --answerclean No --nodiffmenu 1> /dev/null
+yay -Sqy timeshift --noconfirm --answerclean No --nodiffmenu &> /dev/null
 
 echo ===== Install aws-azure-login
-yay -Sqy aws-azure-login --noconfirm --answerclean No --nodiffmenu 1> /dev/null
+yay -Sqy aws-azure-login --noconfirm --answerclean No --nodiffmenu &> /dev/null
 
 
 EOSU
