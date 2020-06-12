3ec3402e0194e9146c252a9b22500b0e26539b3e
hansbarnard <johannes.barnard@clarivate.com>
2020-06-12 08:28:14 +0100

    ddd

--- configure-linux.sh
+++ configure-linux.sh
@@ -7,6 +7,7 @@ cp post-chroot.sh /mnt/scripts
 cp install-gnome.sh /mnt/scripts
 cp gnome-packages-selection.txt /mnt/scripts
 cp gnome-extra-packages-selection.txt /mnt/scripts
+cp install-deepin.sh /mnt/scripts
 cp deepin-extra-packages-selection.txt /mnt/scripts
 cp user-install.sh /mnt/scripts
 
