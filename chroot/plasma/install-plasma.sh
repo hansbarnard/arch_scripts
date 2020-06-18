c89211223f7ef5035448c2c48f554f014788d3dd
hansbarnard <hansbarnard@outlook.com>
2020-06-18 21:20:43 +0100

    xxx

--- chroot/plasma/install-plasma.sh
+++ chroot/plasma/install-plasma.sh
@@ -7,6 +7,9 @@ echo "===== Installing plasma selected"
 selected_packages=$(grep -i $'^Y\s' /scripts/plasma/plasma-packages-selection.txt | awk -v ORS=' ' -F'\t' '{print $2}' )
 pacman -S $selected_packages --needed --noconfirm 1>/dev/null
 
+echo "===== Installing kde apps selected"
+selected_packages=$(grep -i $'^Y\s' /scripts/plasma/kde-applications-selection.txt | awk -v ORS=' ' -F'\t' '{print $2}' )
+pacman -S $selected_packages --needed --noconfirm 1>/dev/null
 
 
 
