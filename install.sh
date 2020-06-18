75dfc0172fa0416bbf852176cc2ac17df83b6e6d
hansbarnard <hansbarnard@outlook.com>
2020-06-18 23:37:28 +0100

    xxx

--- install.sh
+++ install.sh
@@ -9,18 +9,18 @@ then
   exit 1
 fi
 
-if ! ./isoroot/pre-install.sh
-then
-  echo "===== Initialization failed. Exiting..."
-  exit 1
-fi
-
-
-if ! ./isoroot/partitions-install.sh $drive
-then
-  echo "===== Creating partitions failed. Exiting..."
-  exit 1
-fi
+#if ! ./isoroot/pre-install.sh
+#then
+#  echo "===== Initialization failed. Exiting..."
+#  exit 1
+#fi
+#
+#
+#if ! ./isoroot/partitions-install.sh $drive
+#then
+#  echo "===== Creating partitions failed. Exiting..."
+#  exit 1
+#fi
 
 if ! ./isoroot/linux-install.sh $drive
 then
